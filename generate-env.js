73706c98af677017de4dda3eff28c1a72821f775
ritik yadav <yadavritik467@gmail.com>
2024-05-11 12:17:55 +0530

    fix: upadate command & filepath

--- generate-env.js
+++ generate-env.js
@@ -1,32 +1,14 @@
 const fs = require('fs');
 
-// Read the template env file
-fs.readFile('src/environments/environment.sample.ts', 'utf8', (err, data) => {
+// Read the content of environment.sample file
+const filePath = 'src/environments/environment.sample.ts';
+let content = fs.readFileSync(filePath, 'utf8');
 
-  if (err) {
-    console.error('Error reading template file:', err);
-    return;
-  }
+// Replace the placeholder with the actual value of GITHUB_TOKEN
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN || 'YOUR_DEFAULT_VALUE';
+content = content.replace('{{GITHUB_TOKEN}}', GITHUB_TOKEN);
 
-  // Replace placeholders with actual environment variable values
-  const environment = data.replace(/##GITHUB_TOKEN##/g, process.env.GITHUB_TOKEN || 'default-token');
-  // Add more replacements as needed for other environment variables
+// Write the modified content to environment.prod.ts
+fs.writeFileSync('src/environments/environment.prod.ts', content);
 
-  // Write the environment file on the go .prod file
-  fs.writeFile('src/environments/environment.prod.ts', environment, 'utf8', err => {
-    if (err) {
-      console.error('Error writing environment file:', err);
-      return;
-    }
-    console.log('Environment file generated successfully!');
-  });
-
-  // Write the environment file on the go .ts file
-  fs.writeFile('src/environments/environment.ts', environment, 'utf8', err => {
-    if (err) {
-      console.error('Error writing environment file:', err);
-      return;
-    }
-    console.log('Environment file generated successfully!');
-  });
-});
+console.log('Environment file generated successfully!');
