9c1b7da70e40282ac730957f9f80862f0e6b8dc9
ritik yadav <yadavritik467@gmail.com>
2024-05-11 12:54:59 +0530

    fix: fix the env file

---
+++ mynode.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+const dotenv = require('dotenv').config({path: 'src/.env'}); ;
+
+const envFile = `export const environment = {
+    github_token: '${process.env.GITHUB_TOKEN}',
+};
+`;
+const targetPath = path.join(__dirname, './src/environments/environment.sample.ts');
+fs.writeFile(targetPath, envFile, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log(successColor, `${checkSign} Successfully generated environment.sample.ts`);
+    }
+});
